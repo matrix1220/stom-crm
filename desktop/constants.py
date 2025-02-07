086e9bff7259524c8d8f3f1ac1bdced557e29279
The Matrix <matrix_1220@mail.ru>
2025-02-07 11:10:35 +0500

    percentage fix

--- desktop/constants.py
+++ desktop/constants.py
@@ -1,6 +1,6 @@
 
 percentage = {
-    "Xamza": 0.5, "Shavkat": 0.5, "UZI": 0.5, "Sadulla": 0.5, "Raximbergan": 0.5, "Oybek": 0.5,
+    "Xamza": 0.5, "Shavkat": 0.5, "UZI": 0.45, "Sadulla": 0.5, "Raximbergan": 0.5, "Oybek": 0.5,
     "Azamat": 0.5, "Bobur": 0.5, "Sherzod": 0.5, "Odilbek": 0.5, "Otabek": 0.5, "Ra'no": 0.5,
     "Baxrom": 0.5, "Abdulla": 0.5, "Orif": 0.5, "Jaloladdin": 0.5
 }
